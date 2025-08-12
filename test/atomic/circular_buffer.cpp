90256d18f0ac43e2c038af1cf82ad57d235b339f
Hackerl <490021209@qq.com>
2025-08-12 17:23:25 +0800

    feat(tests): enhance lock-free circular buffer tests for improved coverage and clarity

--- test/atomic/circular_buffer.cpp
+++ test/atomic/circular_buffer.cpp
@@ -2,43 +2,102 @@
 #include <zero/atomic/circular_buffer.h>
 
 TEST_CASE("lock-free circular buffer", "[atomic::circular_buffer]") {
-    zero::atomic::CircularBuffer<int> buffer{10};
+    const auto capacity = GENERATE(2uz, take(1, random(3uz, 1024uz)));
+    const auto element = GENERATE(take(1, randomString(1, 1024)));
 
-    REQUIRE(buffer.empty());
-    REQUIRE(buffer.empty());
-    REQUIRE_FALSE(buffer.full());
+    zero::atomic::CircularBuffer<std::string> buffer{capacity};
 
-    SECTION("producer/consumer") {
-        auto index = buffer.reserve();
-        REQUIRE(index);
-        REQUIRE_FALSE(buffer.empty());
-        REQUIRE(buffer.size() == 1);
-        REQUIRE_FALSE(buffer.full());
+    SECTION("size") {
+        const auto size = GENERATE_REF(take(1, random(0uz, capacity - 1)));
 
-        buffer[*index] = 1;
-        buffer.commit(*index);
-
-        index = buffer.acquire();
-        REQUIRE(index);
-        REQUIRE(buffer.empty());
-        REQUIRE_FALSE(buffer.full());
-
-        REQUIRE(buffer[*index] == 1);
-        buffer.release(*index);
-    }
-
-    SECTION("full buffer") {
-        for (std::size_t i{0}; i < 9; i++) {
+        for (std::size_t i{0}; i < size; ++i) {
             const auto index = buffer.reserve();
             REQUIRE(index);
-
-            buffer[*index] = 1;
+            buffer[*index] = element;
             buffer.commit(*index);
         }
 
-        REQUIRE_FALSE(buffer.reserve());
-        REQUIRE_FALSE(buffer.empty());
-        REQUIRE(buffer.size() == 9);
-        REQUIRE(buffer.full());
+        REQUIRE(buffer.size() == size);
+    }
+
+    SECTION("capacity") {
+        REQUIRE(buffer.capacity() == capacity);
+    }
+
+    SECTION("empty") {
+        SECTION("empty") {
+            REQUIRE(buffer.empty());
+        }
+
+        SECTION("not empty") {
+            const auto index = buffer.reserve();
+            REQUIRE(index);
+            buffer[*index] = element;
+            buffer.commit(*index);
+            REQUIRE_FALSE(buffer.empty());
+        }
+    }
+
+    SECTION("full") {
+        SECTION("not full") {
+            REQUIRE_FALSE(buffer.full());
+        }
+
+        SECTION("full") {
+            for (std::size_t i{0}; i < capacity - 1; ++i) {
+                const auto index = buffer.reserve();
+                REQUIRE(index);
+                buffer[*index] = element;
+                buffer.commit(*index);
+            }
+
+            REQUIRE(buffer.full());
+        }
+    }
+
+    SECTION("subscript access") {
+        {
+            const auto index = buffer.reserve();
+            REQUIRE(index);
+            buffer[*index] = element;
+            buffer.commit(*index);
+        }
+
+        const auto index = buffer.acquire();
+        REQUIRE(index);
+        REQUIRE(buffer[*index] == element);
+        buffer.release(*index);
+    }
+
+    SECTION("reserve") {
+        SECTION("success") {
+            REQUIRE(buffer.reserve() == 0);
+        }
+
+        SECTION("failure") {
+            for (std::size_t i{0}; i < capacity - 1; ++i) {
+                const auto index = buffer.reserve();
+                REQUIRE(index);
+            }
+
+            REQUIRE_FALSE(buffer.reserve());
+        }
+    }
+
+    SECTION("acquire") {
+        SECTION("success") {
+            {
+                const auto index = buffer.reserve();
+                REQUIRE(index);
+                buffer[*index] = element;
+                buffer.commit(*index);
+            }
+
+            REQUIRE(buffer.acquire());
+        }
+
+        SECTION("failure") {
+            REQUIRE_FALSE(buffer.acquire());
+        }
     }
 }
